2002b09dfaab9c6352857d8021c721129e991c00
bsmithNeumont <brsmith@student.neumont.edu>
2023-10-05 23:23:36 -0600

    Finished MERN stack backend

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,3 +1,5 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
 let restaurants
 
 export default class RestaurantsDAO {
@@ -52,6 +54,61 @@ export default class RestaurantsDAO {
                 `Unable to convert cursor to array or problem counting documents, ${e}`
             )
             return { restaurantsList: [], totalNumRestaurants: 0 }
+        }
+    }
+
+    static async getRestaurantById(id) {
+        try {
+            const pipeline = [
+                {
+                    $match: {
+                        _id: new ObjectId(id),
+                    },
+                },
+                {
+                    $lookup: {
+                        from: "reviews",
+                        let: {
+                            id: "$_id",
+                        },
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ["$restaurant_id", "$$id"],
+                                    },
+                                },
+                            },
+                            {
+                                $sort: {
+                                    date: -1,
+                                },
+                            },
+                        ],
+                        as: "reviews",
+                    },
+                },
+                {
+                    $addFields: {
+                        reviews: "$reviews",
+                    },
+                },
+            ]
+            return await restaurants.aggregate(pipeline).next()
+        } catch (e) {
+            console.error(`Something went wrong in getRestaurantByID: ${e}`)
+            throw e
+        }
+    }
+
+    static async getCuisines() {
+        let cuisines = []
+        try {
+            cuisines = await restaurants.distinct("cuisine")
+            return cuisines
+        } catch (e) {
+            console.error(`Unable to get cuisines, ${e}`)
+            return cuisines
         }
     }
 }
