b73550fe2897fb41ef1c1b2ed11d7bf224059c13
Brandon Smith <brsmith@student.neumont.edu>
2024-07-22 21:42:17 -0600

    Hosting on Render.com

--- QTFrontend/src/constants.js
+++ QTFrontend/src/constants.js
@@ -1,7 +1,7 @@
 // Constants.js
-const productionFrontend = "http://localhost:3000";
+const productionFrontend = "https://questtrackr.onrender.com";
 const developmentFrontend = "http://localhost:3000";
-const productionBackend = "http://localhost:5000";
+const productionBackend = "https://questtrackrbackend.onrender.com";
 const developmentBackend = "http://localhost:5000";
 export const config =
   process.env.NODE_ENV === "development"
